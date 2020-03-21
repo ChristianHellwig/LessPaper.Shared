ee4d461bccd7e446fd94fb7801b983e9c2b354f4
benediktbroich <b.broich@posteo.de>
2020-03-21 18:14:20 +0100

    Minimal Directory Metadata changed

--- LessPaper.Shared/Interfaces/General/IMinimalDirectoryMetadata.cs
+++ LessPaper.Shared/Interfaces/General/IMinimalDirectoryMetadata.cs
@@ -8,15 +8,5 @@ namespace LessPaper.Shared.Interfaces.General
         /// Number of total childs in the directory
         /// </summary>
         uint NumberOfChilds { get; }
-
-        /// <summary>
-        /// Date of last change
-        /// </summary>
-        DateTime LatestChangeDate { get; }
-
-        /// <summary>
-        /// Date of last view
-        /// </summary>
-        DateTime LatestViewDate { get; }
     }
 }
