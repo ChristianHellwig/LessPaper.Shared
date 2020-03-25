a2142c87f1fc6707500d1d6ecee3eb3928b6a0a7
Christian Hellwig <hellwig.christian@gmx.de>
2020-03-25 15:13:28 +0100

    Add necessary methods to implement the writer service

---
+++ LessPaper.Shared/Interfaces/Buckets/IBucket.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace LessPaper.Shared.Interfaces.Buckets
+{
+    public interface IBucket
+    {
+
+        /// <summary>
+        /// Encrypt and upload file
+        /// </summary>
+        /// <param name="bucketName">Name of the Bucket</param>
+        /// <param name="fileId">Unique random file id</param>
+        /// <param name="fileSize">File size in bytes</param>
+        /// <param name="plaintextKey">Plaintext encryption key</param>
+        /// <param name="fileStream">Read-File-Stream</param>
+        /// <returns>True if successful</returns>
+        Task<bool> UploadFileEncrypted(string bucketName, string fileId, int fileSize, byte[] plaintextKey, Stream fileStream);
+
+
+        /// <summary>
+        /// Upload file
+        /// </summary>
+        /// <param name="bucketName">Name of the Bucket</param>
+        /// <param name="fileId">Unique random file id</param>
+        /// <param name="fileSize">File size in bytes</param>
+        /// <param name="fileStream">Read-File-Stream</param>
+        /// <returns>True if successful</returns>
+        Task<bool> UploadFile(string bucketName, string fileId, int fileSize, Stream fileStream);
+
+
+        /// <summary>
+        /// Download file from bucket
+        /// </summary>
+        /// <param name="bucketName">Name of the Bucket</param>
+        /// <param name="fileId">Unique random file id</param>
+        /// <param name="fileSize">File size in bytes</param>
+        /// <param name="fileStream">Write-File-Stream</param>
+        /// <returns>True if successful</returns>
+        Task<bool> DownloadFile(string bucketName, string fileId, int fileSize, Stream fileStream);
+
+
+        /// <summary>
+        /// Download file from bucket and decrypt
+        /// </summary>
+        /// <param name="bucketName">Name of the Bucket</param>
+        /// <param name="fileId">Unique random file id</param>
+        /// <param name="fileSize">File size in bytes</param>
+        /// <param name="plaintextKey">Plaintext decryption key</param>
+        /// <param name="fileStream">Write-File-Stream</param>
+        /// <returns>True if successful</returns>
+        Task<bool> DownloadFileDecrypted(string bucketName, string fileId, int fileSize, byte[] plaintextKey, Stream fileStream);
+    }
+}
--- LessPaper.Shared/Interfaces/GuardApi/IGuardApi.cs
+++ LessPaper.Shared/Interfaces/GuardApi/IGuardApi.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using LessPaper.Shared.Enums;
+using LessPaper.Shared.Interfaces.General;
+using LessPaper.Shared.Interfaces.GuardApi.Response;
+using LessPaper.Shared.Interfaces.WriteApi.WriteObjectApi;
 
 namespace LessPaper.Shared.Interfaces.GuardApi
 {
@@ -25,5 +29,60 @@ namespace LessPaper.Shared.Interfaces.GuardApi
         /// <returns>Necessary credentials or null if user does not exist</returns>
         /// <exception cref="InvalidOperationException">Throws if service not available</exception>
         Task<ICredentialsResponse> GetUserCredentials(string email);
+
+        /// <summary>
+        /// Get permissions of an array of files/directories
+        /// </summary>
+        /// <param name="userId">User id</param>
+        /// <param name="objectIds">Requested object ids</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">Throws if service not available</exception>
+        Task<IPermissionResponse> GetObjectsPermissions(string userId, string[] objectIds);
+
+        /// <summary>
+        /// Add a directory to a directory
+        /// </summary>
+        /// <param name="parentDirectoryId">Parent directory id i.e. the root directory</param>
+        /// <param name="directoryName">New directory name</param>
+        /// <param name="newDirectoryId">Unique id of the directory</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">Throws if service not available</exception>
+        Task<bool> AddDirectory(string parentDirectoryId, string directoryName, string newDirectoryId);
+
+        /// <summary>
+        /// Deletes an file or an directory
+        /// </summary>
+        /// <param name="objectId">File or directory id</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">Throws if service not available</exception>
+        Task<bool> DeleteObject(string objectId);
+
+        /// <summary>
+        /// Add a new file to a directory
+        /// </summary>
+        /// <param name="directoryId">Directory Id</param>
+        /// <param name="fileId">File id</param>
+        /// <param name="fileSize">Size/Length of the file</param>
+        /// <param name="encryptedKey">Encrypted key</param>
+        /// <param name="documentLanguage">Language of the document</param>
+        /// <param name="fileExtension">Type of the file</param>
+        /// <returns>Quick number</returns>
+        /// <exception cref="InvalidOperationException">Throws if service not available</exception>
+        Task<int> AddFile(
+            string directoryId,
+            string fileId,
+            int fileSize, 
+            string encryptedKey, 
+            DocumentLanguage documentLanguage, 
+            ExtensionType fileExtension);
+
+        /// <summary>
+        /// Update the metadata of an object i.e. the directory name
+        /// </summary>
+        /// <param name="objectId">ObjectID of the Object where the metadata should be updated</param>
+        /// <param name="updatedMetadata">New metadata</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">Throws if service not available</exception>
+        Task<bool> UpdateObjectMetadata(string objectId, IMetadataUpdate updatedMetadata);
     }
 }
